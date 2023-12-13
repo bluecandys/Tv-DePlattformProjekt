46b79c88888e6bd08912b9e21faa8b9a16db5081
Emilia Melander <emiliamelander00@gmail.com>
2023-12-13 15:09:32 +0100

    character moves

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
 
     private float movementInputDirection;
+
+    private bool isFacingRight = true;
     private Rigidbody2D rb;
+
+    public float moveSpeed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +22,40 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckInput();
+        CheckMovementDirection();
     }
 
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+    }
+
+    private void CheckMovementDirection()
+    {
+        if(isFacingRight && movementInputDirection < 0)
+        {
+            Flip();
+        }
+        else if (!isFacingRight && movementInputDirection > 0)
+        {
+            Flip();
+        }
+    }
+    
     private void CheckInput()
     {
         movementInputDirection = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void ApplyMovement()
+    {
+        rb.velocity = new Vector2(moveSpeed * movementInputDirection, rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
     }
 }
